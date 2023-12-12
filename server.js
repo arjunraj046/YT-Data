779bad2b01aa30952828565ce7a7ab9ec43ec1d4
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2023-12-12 11:13:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const { google } = require('googleapis');
-const axios = require('axios');
+const express = require('express')
+const { google } = require('googleapis')
+const axios = require('axios')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-let accessToken;
-let refreshToken;
+let accessToken
+let refreshToken
 
-const ownerID = "Jgrl-IYF1196ZxijRcZLXQ";
-const CLIENT_ID = '327277406160-7oddheciuo5m459o6cfqqobf7cclhnmp.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URL = 'https://yt-data.onrender.com/oauth2callback';
-let baseURL = "https://www.googleapis.com/youtube/partner/v1/";
-const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+const ownerID = "Jgrl-IYF1196ZxijRcZLXQ"
+const CLIENT_ID = '327277406160-7oddheciuo5m459o6cfqqobf7cclhnmp.apps.googleusercontent.com'
+const CLIENT_SECRET = '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'
+const REDIRECT_URL = 'https://yt-data.onrender.com/oauth2callback'
+let baseURL = "https://www.googleapis.com/youtube/partner/v1/"
+const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 const scopes = [
   'https://www.googleapis.com/auth/youtube.readonly',
   'https://www.googleapis.com/auth/youtube',
   'https://www.googleapis.com/auth/yt-analytics.readonly',
   'https://www.googleapis.com/auth/yt-analytics-monetary.readonly'
-];
- 
+]
+
 const refreshAccessToken = async () => {
   console.log("Creating Refresh Token 🤩🤩🤩");
   try {
@@ -41,6 +41,7 @@ const checkAccessToken = async (req, res, next) => {
   }
   next();
 };
+
 // creating auth url for the client 
 app.get('/auth-url', (req, res) => {
   console.log("Google Auth");
@@ -51,10 +52,10 @@ app.get('/auth-url', (req, res) => {
   });
   res.send({ url: authUrl });
 });
-// Catch the tokens with redirect url ...
+
+// Catch the tokens with redirect url 
 app.get('/oauth2callback', async (req, res) => {
   const { code } = req.query;
-
   try {
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
@@ -71,6 +72,7 @@ app.get('/oauth2callback', async (req, res) => {
     res.status(500).send('Authorization failed!');
   }
 });
+
 // fetch the types of reports 
 app.get('/youtube-report-types', checkAccessToken, async (req, res) => {
   try {
@@ -89,6 +91,7 @@ app.get('/youtube-report-types', checkAccessToken, async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch report types' });
   }
 });
+
 // response get the listed job which already created 
 app.get('/list-jobs', checkAccessToken, async (req, res) => {
   try {
@@ -108,6 +111,7 @@ app.get('/list-jobs', checkAccessToken, async (req, res) => {
     res.status(500).json({ error: 'Failed to list jobs' });
   }
 });
+
 // list jobs response hard code 
 const jobData = {
   "jobs": [
@@ -118,25 +122,7 @@ const jobData = {
           "createTime": "2023-12-09T14:12:49Z"
       }
   ]
-};
-// app.get('/job-details/:jobId', checkAccessToken, async (req, res) => {
-//   try {
-//     const jobId = req.params.jobId; // Extract the job ID from the request params
-//     const response = await axios.get(`https://youtubereporting.googleapis.com/v1/jobs/${jobId}`, {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//       },
-//       params: {
-//         onBehalfOfContentOwner: ownerID,
-//       },
-//     });
-//     console.log('Job Details:', response.data);
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error('Error fetching job details:', error.response.data);
-//     res.status(500).json({ error: 'Failed to fetch job details' });
-//   }
-// });
+}
 
 // route to create a new job 
 app.get('/create-job', checkAccessToken, async (req, res) => {
@@ -166,12 +152,12 @@ app.get('/create-job', checkAccessToken, async (req, res) => {
     res.status(500).json({ error: 'Failed to create job' });
   }
 });
+
 // with job id and owner id fetching job report 
 app.get('/get-report', checkAccessToken, async (req, res) => {
   try {
     const reportId = "content_owner_estimated_revenue_a1";
     const jobId = "4e055ed3-a7f3-41c8-b045-7a98b665bba6";
-
     const response = await axios.get(`https://youtubereporting.googleapis.com/v1/jobs/${jobId}/reports/${reportId}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -189,10 +175,12 @@ app.get('/get-report', checkAccessToken, async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch report' });
   }
 });
+
 // 404
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
